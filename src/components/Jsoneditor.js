630ce17e26cf99d0efb7c45906f0ad1d24e25b66
Patryk Karwowski <Patryk.Karwowski@unicorntraining.com>
2018-06-21 15:23:30 +0100

    setup files structure

---
+++ src/components/Jsoneditor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Jsoneditor.css';
+
+class Jsoneditor extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default Jsoneditor;
